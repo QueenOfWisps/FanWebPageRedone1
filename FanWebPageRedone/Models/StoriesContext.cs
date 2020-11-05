5796b6238da7e488117094ca2da5964269329838
QueenOfWisps <brittainr@my.lanecc.edu>
2020-11-04 18:16:48 -0800

    gettingstartedpt2
    this is starting out

---
+++ FanWebPageRedone/Models/StoriesContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FanWebPageRedone.Models
+{
+    public class StoriesContext : DbContext
+    {
+        public StoriesContext(
+            DbContextOptions<StoriesContext>options) : base(options) { }
+
+        public DbSet<Story> Topic { get; set;  }
+        public DbSet<User>  User  { get; set; }
+
+    }
+}
